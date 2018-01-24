c8ded1c2407d90bfcfa980340427ebd7201c8ef2
Jose Eduardo Serrano <joser_isi@hotmail.com>
2018-01-24 11:46:44 -0600

    Se configuro la primera vista y el loggeo para la aplicacion

--- mmtool/empleos/views.py
+++ mmtool/empleos/views.py
@@ -1,18 +1,23 @@
 from django.http import HttpResponse
+from django.template import loader
 from empleos import services
+import logging
+import logging.config
 
 
 #def home(request):
 #    return HttpResponse('Home de las listas de ofertas de empleos')
-
+logger = logging.getLogger(__name__)
 
 def home(request):
-	categorias_name = []
+	#categorias_name = []
 	categorias =  services.get_categorias()
-
-	for categoria in categorias:
-		categorias_name.append(categoria.name)
-		response_html = '<br>'.join(categorias_name)
-	return HttpResponse(response_html)
+	#latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	logger.debug(categorias)
+	template = loader.get_template('empleos/home.html')
+	context = {
+	'categorias': categorias,
+	}
+	return HttpResponse(template.render(context, request))
 
  
--- mmtool/mmtool/settings.py
+++ mmtool/mmtool/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import logging
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,3 +121,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    # will output to your console
+    logging.basicConfig(
+        level = logging.DEBUG,
+        format = '%(asctime)s %(levelname)s %(message)s',
+    )
+else:
+    # will output to logging file
+    logging.basicConfig(
+        level = logging.DEBUG,
+        format = '%(asctime)s %(levelname)s %(message)s',
+        filename = '/my_log_file.log',
+        filemode = 'a'
+    )
